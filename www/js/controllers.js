447c109b004effb7330fb01988760fbabc61fc74
Marylène Debavelaere <Marylne Debavelaere>
2017-06-27 11:11:00 +0200

    add search and add word

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -12,15 +12,22 @@ angular.module('starter.controllers', [])
 
   $scope.addWord = {};
   $scope.searchWord = {};
+  var parameter = JSON.stringify({type:"word", label:+$scope.addWord.word});
 
     // Perform the login action when the user submits the login form
   $scope.doAdd = function() {
-    console.log('word added', $scope.addWord);
+    console.log('word added', $scope.addWord.word);
+     var parameter = "{'word' : {	'label' : '"+$scope.addWord.word+"'}}"
+    $http.post('http://172.20.10.14:8080/server/remote/services/words',parameter).
+    then(function(response) {
+            $scope.greeting = response.data;
+            console.log(response)
+        });
   };
 
     $scope.doSearch = function() {
     console.log('word search', $scope.searchWord.word);
-        $http.delete('http://10.162.128.70:8080/server/remote/services/words/1').
+        $http.get('http://172.20.10.14:8080/server/remote/services/words?contains='+$scope.searchWord.word).
         then(function(response) {
             $scope.greeting = response.data;
             console.log(response)
